47ef9d3183041ebc93660f5d0a736250257c686b
david <david>
1993-01-01 11:19:36 +0000

    *** empty log message ***

--- User_interface/main/main.c
+++ User_interface/main/main.c
@@ -1,8 +1,8 @@
 #include  <def_register.h>
 
-#define  X_SIZE   256
-#define  Y_SIZE   256
-#define  Z_SIZE   256
+#define  X_SIZE   100
+#define  Y_SIZE   100
+#define  Z_SIZE   100
 
 #define  N_VALUES   256
 
@@ -16,11 +16,12 @@ main()
     int              x_stride, y_stride;
     int              a, a1, a2, y_image, sizes[3];
     int              a_indices[3], a1_indices[3], a2_indices[3];
+    Real             x_start, y_start;
     Pixel_types      pixel_type;
     pixels_struct    pixels[3][3];
     Boolean          interpolation_flag;
-    Volume_type      volume[X_SIZE*Y_SIZE*Z_SIZE];
     Volume_type      *volume_start;
+    Volume_type      volume[X_SIZE*Y_SIZE*Z_SIZE];
     Colour           rgb_colour_map[N_VALUES];
     Real             thickness[3];
     Real             start_time, compute_time, render_time;
@@ -32,19 +33,26 @@ main()
     sizes[Y] = Y_SIZE;
     sizes[Z] = Z_SIZE;
 
+    thickness[X] = 1.0;
+    thickness[Y] = 1.5;
+    thickness[Z] = 2.0;
+
     for_less( i, 0, N_VALUES )
         rgb_colour_map[i] = make_Colour( i, i, i );
 
     for_less( x, 0, X_SIZE )
     {
-        dx = 2.0 * (Real) x / X_SIZE - 1.0;
+        dx = 2.0 * (Real) x / (Real) X_SIZE - 1.0;
+        if( dx < 0.0 ) dx = -dx;
         for_less( y, 0, Y_SIZE )
         {
-            dy = 2.0 * (Real) y / Y_SIZE - 1.0;
+            dy = 2.0 * (Real) y / (Real) Y_SIZE - 1.0;
+            if( dy < 0.0 ) dy = -dy;
             for_less( z, 0, Z_SIZE )
             {
-                dz = 2.0 * (Real) z / Z_SIZE - 1.0;
-                val = (int) ((ABS(dx) + ABS(dy) + ABS(dz)) * N_VALUES);
+                dz = 2.0 * (Real) z / (Real) Z_SIZE - 1.0;
+                if( dz < 0.0 ) dz = -dz;
+                val = (int) ((dx + dy + dz) * (Real) N_VALUES / 2.0 );
                 if( val > N_VALUES-1 )  val = 0;
                 volume[IJK(x,y,z,Y_SIZE,Z_SIZE)] = val;
             }
@@ -97,18 +105,22 @@ main()
             y_stride = IJK(a2_indices[X],a2_indices[Y],a2_indices[Z],Y_SIZE,Z_SIZE) -
                        IJK(0,0,0,Y_SIZE,Z_SIZE);
 
-            a_indices[X] = 0;
-            a_indices[Y] = 0;
-            a_indices[Z] = 0;
-            a_indices[a] = (sizes[a] / 2 + slice_offset + y_image) % sizes[a];
-            volume_start = &volume[IJK(a_indices[X],a_indices[Y],a_indices[Z],
-                                   Y_SIZE,Z_SIZE)];
-
             status = initialize_pixels( &pixels[a][y_image], sizes[a1],
                                         sizes[a2], pixel_type );
 
+            a_indices[X] = 0;
+            a_indices[Y] = 0;
+            a_indices[Z] = 0;
+            a_indices[a] = sizes[a] / 2.0;
+            volume_start = &volume[IJK(a_indices[X],a_indices[Y],a_indices[Z],
+                                       Y_SIZE,Z_SIZE)];
+
+            x_start = -0.5;
+            y_start = -0.5;
+
             render_volume_to_slice( volume_start, x_stride, y_stride,
-                                    1.0, 1.0,
+                                    x_start, y_start,
+                                    thickness[a1], thickness[a2],
                                     interpolation_flag, rgb_colour_map,
                                     colour_index_offset,
                                     &pixels[a][y_image] );
