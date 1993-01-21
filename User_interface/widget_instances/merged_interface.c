8443dfb06d5d056530b378b382a0e692ea2cf52a
david <david>
1993-01-21 13:39:53 +0000

    *** empty log message ***

--- User_interface/widget_instances/merged_interface.c
+++ User_interface/widget_instances/merged_interface.c
@@ -23,38 +23,42 @@ public  void  add_merged_widgets(
     UI_struct         *ui_info,
     Viewport_types    viewport_index )
 {
-    int            x, y, height, volume_index;
-
-    volume_index = get_viewport_volume_index( viewport_index );
+    int            x, y, height;
 
     x = Volume_menu_x_offset;
     y = Volume_menu_y_offset;
 
-    widget_indices[OPACITY_2_SLIDER] = create_slider( ui_info,
+    widget_indices[OPACITY_2_SLIDER] = add_widget_to_list(
+                   &ui_info->widget_list[viewport_index],
+                   create_slider( &ui_info->graphics_window,
                    viewport_index, x, y, Opacity_slider_width,
                    Opacity_slider_height,
                    0.5, 0.0, 1.0,
                    OFF,
                    SLIDER_ACTIVE_COLOUR, SLIDER_INACTIVE_COLOUR,
                    SLIDER_PEG_COLOUR,
-                   opacity_2_callback );
+                   opacity_2_callback, (void *) NULL ) );
 
     y += Opacity_slider_height + Volume_button_height +
          Interface_y_spacing;
 
-    widget_indices[OPACITY_1_SLIDER] = create_slider( ui_info,
+    widget_indices[OPACITY_1_SLIDER] = add_widget_to_list(
+                   &ui_info->widget_list[viewport_index],
+                   create_slider( &ui_info->graphics_window,
                    viewport_index, x, y, Opacity_slider_width,
                    Opacity_slider_height,
                    0.5, 0.0, 1.0,
                    OFF,
                    SLIDER_ACTIVE_COLOUR, SLIDER_INACTIVE_COLOUR,
                    SLIDER_PEG_COLOUR,
-                   opacity_1_callback );
+                   opacity_1_callback, (void *) NULL ) );
 
     y += Opacity_slider_height + Volume_button_height +
          Interface_y_spacing;
 
-    widget_indices[RESET_VIEW_BUTTON] = create_button( ui_info,
+    widget_indices[RESET_VIEW_BUTTON] = add_widget_to_list(
+                   &ui_info->widget_list[viewport_index],
+                   create_button( &ui_info->graphics_window,
                viewport_index, x, y,
                Volume_button_width, Volume_button_height,
                "Reset View",
@@ -62,34 +66,29 @@ public  void  add_merged_widgets(
                BUTTON_INACTIVE_COLOUR,
                BUTTON_PUSHED_COLOUR, BUTTON_TEXT_COLOUR,
                Button_text_font, Button_text_font_size,
-               reset_view_callback );
+               reset_view_callback, (void *) NULL ) );
 
     y += Volume_button_height + Interface_y_spacing;
 
-    ui_info->position_text_start_index[volume_index] =
+    ui_info->position_text_start_index[2] =
                                 add_cursor_position_widgets(
                                 ui_info, viewport_index, x, y, &height );
 }
 
 public  void  set_merged_widgets_activity(
     UI_struct         *ui_info,
-    int               volume_index,
     Boolean           activity )
 {
     Merged_widgets       widgets;
-    Viewport_types       viewport_index;
-
-    viewport_index = get_volume_menu_viewport_index( volume_index );
 
     for_enum( widgets, N_MERGED_WIDGETS, Merged_widgets )
     {
-        set_widget_activity_and_update( ui_info,
-                             ui_info->widget_list[viewport_index].widgets
+        set_widget_activity( ui_info->widget_list[Merged_menu_viewport].widgets
                                           [widget_indices[widgets]],
                              activity );
     }
 
-    set_voxel_position_widgets_activity( ui_info, viewport_index,
-                               ui_info->position_text_start_index[volume_index],
+    set_voxel_position_widgets_activity( ui_info, Merged_menu_viewport,
+                               ui_info->position_text_start_index[2],
                                activity );
 }
