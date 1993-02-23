0dee806886399b1c42cacb6986862e0b893f5207
david <david>
1993-02-23 14:02:47 +0000

    *** empty log message ***

--- User_interface/event_handling/event_loop.c
+++ User_interface/event_handling/event_loop.c
@@ -35,16 +35,28 @@ private  void   handle_event(
     window_struct            *event_window,
     int                      key_pressed )
 {
+    static                  shift_state = OFF;
     int                     x_mouse, y_mouse;
     event_viewports_struct  *event_viewports;
 
-    if( !execute_global_event_callbacks( event_type, key_pressed ) &&
+    if( (event_type == KEY_DOWN_EVENT || event_type == KEY_UP_EVENT) &&
+        (key_pressed == LEFT_SHIFT_KEY ||
+         key_pressed == RIGHT_SHIFT_KEY ||
+         key_pressed == LEFT_CTRL_KEY ||
+         key_pressed == RIGHT_CTRL_KEY ||
+         key_pressed == LEFT_ALT_KEY ||
+         key_pressed == RIGHT_ALT_KEY) )
+    {
+        shift_state = (event_type == KEY_DOWN_EVENT);
+    }
+    else if( !execute_global_event_callbacks( shift_state, event_type,
+                                              key_pressed ) &&
         event_is_allowable( event_type ) &&
         (!mouse_must_be_in_window(event_type) ||
          G_get_mouse_position( event_window, &x_mouse, &y_mouse )) &&
         lookup_event_viewports( event_window, &event_viewports ) )
     {
-        execute_event_viewport_events( event_viewports,
+        execute_event_viewport_events( shift_state, event_viewports,
                                        x_mouse, y_mouse,
                                        event_type, key_pressed );
     }
