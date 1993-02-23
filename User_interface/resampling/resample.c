e30c31760b52d0d7b2fcc9dfdfd9bbc242acb05f
david <david>
1993-02-23 13:33:51 +0000

    *** empty log message ***

--- User_interface/resampling/resample.c
+++ User_interface/resampling/resample.c
@@ -1,92 +1,106 @@
 #include  <def_user_interface.h>
-#include  <signal.h>
-#include  <sys/wait.h>
+
+private  void  delete_popup_interaction(
+    resample_volume_struct    *data );
 
 private  void  done_resampling(
-    int  exit_status )
+    UI_struct               *ui,
+    resample_volume_struct  *data )
 {
-    if( exit_status == 0 )
+    Status  status;
+
+    status = output_volume( get_ui_struct()->resampled_filename,
+                            &data->resampled_volume );
+
+    if( status == OK )
     {
         (void) initialize_loading_volume( get_ui_struct(),
                                           RESAMPLED_VOLUME_INDEX,
                                           get_ui_struct()->resampled_filename,
                                           TRUE );
     }
+
+    delete_popup_interaction( data );
+}
+
+private  DEFINE_EVENT_FUNCTION( more_resampling )   /* ARGSUSED */
+{
+    resample_volume_struct   *data;
+    Real                     fraction_done;
+
+    data = (resample_volume_struct *) callback_data;
+
+    if( do_more_resampling( &data->resample, Seconds_per_resample,
+                            &fraction_done ) )
+    {
+        set_resample_popup_meter( data, fraction_done );
+    }
     else
     {
-        print( "Resampling failed %d\n", exit_status );
+        done_resampling( get_ui_struct(), data );
     }
 }
 
-#ifdef USE_FORK
-private  void  child_died_signal( int sig, int code, struct sigcontext *sc )
+private  void  delete_popup_interaction(
+    resample_volume_struct    *data )
 {
-    int   statptr;
-    int   exit_status;
+    set_resample_button_activity( get_ui_struct(), ON );
 
-    (void) wait( &statptr );
+    remove_global_event_callback( NO_EVENT, more_resampling, (void *) data );
 
-    if( WIFEXITED(statptr) )
-    {
-        exit_status = WEXITSTATUS(statptr);
+    delete_resample_popup( data );
 
-        done_resampling( exit_status );
-    }
+    delete_volume( &data->resampled_volume );
 
-    (void) signal( SIGCLD, child_died_signal );
+    FREE( data );
 }
-#endif
+
+public  void  cancel_resampling(
+    resample_volume_struct *data )
+{
+    delete_popup_interaction( data );
+}
 
 public  void  start_resampling(
     UI_struct   *ui,
-    char        volume_1_filename[],
-    char        volume_2_filename[],
-    char        resampled_filename[],
-    Transform   *resampling_transform )
+    Transform   *resampling_transform,
+    char        resampled_filename[] )
 {
-    int     exit_status;
-    char    command_str[2000];
-    String  output_filename;
-    String  transform_filename;
+    int                     x_min, x_max, y_min, y_max, x_window, y_window;
+    resample_volume_struct  *data;
+    String                  output_filename;
 
-    (void) tmpnam( transform_filename );
+    ALLOC( data, 1 );
+
+    (void) strcpy( ui->original_filename_volume_2,
+                   IF_get_volume_filename(RESAMPLED_VOLUME_INDEX) );
 
     expand_filename( resampled_filename, output_filename );
-
-    if( RESAMPLED_VOLUME_INDEX == 1 )
-        (void) strcpy( ui->original_filename_volume_2, volume_2_filename );
-    else
-        (void) strcpy( ui->original_filename_volume_2, volume_1_filename );
-
     (void) strcpy( ui->resampled_filename, output_filename );
+
     ui->resampling_transform = *resampling_transform;
 
-    IF_save_transform( transform_filename );
+    data->resampled_volume = *IF_get_volume( 1 - RESAMPLED_VOLUME_INDEX );
 
-#ifdef USE_FORK
-    (void) signal( SIGCLD, child_died_signal );
+    alloc_volume( &data->resampled_volume );
 
-    if( fork() == 0 )
-    {
-        (void) execlp( "register_resample",
-                       volume_1_filename,
-                       volume_2_filename,
-                       transform_filename,
-                       output_filename,
-                       (char *) NULL );
-    }
+    initialize_resample_volume( &data->resample,
+                                IF_get_volume( RESAMPLED_VOLUME_INDEX ),
+                                resampling_transform, &data->resampled_volume);
 
     set_resample_button_activity( ui, FALSE );
-#else
-    (void) printf( "Please wait a couple of minutes for the resampling.\n" );
-    (void) sprintf( command_str, "register_resample %s %s %s %s",
-                    volume_1_filename,
-                    volume_2_filename,
-                    transform_filename,
-                    output_filename );
 
-    exit_status = system( command_str );
+    add_global_event_callback( NO_EVENT, more_resampling, ANY_MODIFIER,
+                               (void *) data );
 
-    done_resampling( exit_status );
-#endif
+    get_graphics_viewport( &ui->graphics_window.graphics,
+                           get_volume_menu_viewport_index(
+                                      RESAMPLED_VOLUME_INDEX),
+                           &x_min, &x_max, &y_min, &y_max );
+
+    G_get_window_position( ui->graphics_window.window,
+                           &x_window, &y_window );
+
+    initialize_resample_popup( data, x_window + x_min, y_window + y_min,
+                               resampled_filename );
 }
