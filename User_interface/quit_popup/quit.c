e976fa902777dd62893311e298575791286a8430
david <david>
1993-03-05 13:18:19 +0000

    Initial revision

---
+++ User_interface/quit_popup/quit.c
@@ -0,0 +1,110 @@
+#include  <def_user_interface.h>
+
+private  void  delete_quit_confirm(
+    popup_struct          *popup )
+{
+    delete_popup_window( popup );
+
+    FREE( popup );
+
+    set_quit_button_activity( get_ui_struct(), ON );
+}
+
+private  DEFINE_WIDGET_CALLBACK( quit_button_callback )   /* ARGSUSED */
+{
+    set_quit_program_flag();
+
+    delete_quit_confirm( (popup_struct *) callback_data );
+}
+
+private  DEFINE_WIDGET_CALLBACK( dont_quit_button_callback )   /* ARGSUSED */
+{
+    delete_quit_confirm( (popup_struct *) callback_data );
+}
+
+private  DEFINE_EVENT_FUNCTION( kill_window_callback )   /* ARGSUSED */
+{
+    delete_quit_confirm( (popup_struct *) callback_data );
+}
+
+public  void  popup_quit_confirm(
+    UI_struct   *ui )
+{
+    int               x, y;
+    Point             point;
+    object_struct     *object;
+    text_struct       *text;
+    widget_struct     *widget;
+    popup_struct      *popup;
+
+    set_quit_button_activity( ui, OFF );
+
+    G_get_mouse_screen_position( &x, &y );
+
+    ALLOC( popup, 1 );
+
+    create_popup_window( popup, "Quit Dialog", x, y, Quit_x_size, Quit_y_size,
+                         kill_window_callback, (void *) popup );
+
+    x = Interface_x_spacing;
+    y = Quit_y_size - 1 - Interface_y_spacing;
+
+    if( !IF_tag_points_have_been_saved() )
+    {
+        object = create_object( TEXT );
+        text = (text_struct *) get_object_pointer( object );
+        fill_Point( point, x, y - Message_font_size, 0.0);
+        initialize_text( text, &point, Message_text_colour,
+                         Message_font, Message_font_size );
+
+        (void) strcpy( text->string, "The tag points have not been saved." );
+
+        add_object_to_viewport( &popup->graphics.graphics, 0, NORMAL_PLANES,
+                                object );
+
+        y -= Message_font_size + Interface_y_spacing;
+    }
+
+    object = create_object( TEXT );
+    text = (text_struct *) get_object_pointer( object );
+    fill_Point( point, x, y - Message_font_size, 0.0);
+    initialize_text( text, &point, Message_text_colour,
+                     Message_font, Message_font_size );
+
+    (void) strcpy( text->string, "Do you want to quit?" );
+
+    add_object_to_viewport( &popup->graphics.graphics, 0, NORMAL_PLANES,
+                            object );
+
+    y -= Message_font_size + Interface_y_spacing;
+
+    initialize_widget_list( &popup->widgets );
+
+    y -= Button_height;
+
+    widget = create_button( &popup->graphics, 0,
+                            x, y, Button_width, Button_height,
+                            "Quit", ON, TRUE,
+                            BUTTON_ACTIVE_COLOUR,
+                            BUTTON_SELECTED_COLOUR,
+                            BUTTON_INACTIVE_COLOUR,
+                            BUTTON_PUSHED_COLOUR,
+                            BUTTON_TEXT_COLOUR,
+                            Button_text_font, Button_text_font_size,
+                            quit_button_callback, (void *) popup );
+    (void) add_widget_to_list( &popup->widgets, widget );
+
+    x += Button_width + Interface_x_spacing;
+
+    widget = create_button( &popup->graphics, 0,
+                            x, y, Button_width, Button_height,
+                            "Don't Quit", ON, TRUE,
+                            BUTTON_ACTIVE_COLOUR,
+                            BUTTON_SELECTED_COLOUR,
+                            BUTTON_INACTIVE_COLOUR,
+                            BUTTON_PUSHED_COLOUR,
+                            BUTTON_TEXT_COLOUR,
+                            Button_text_font, Button_text_font_size,
+                            dont_quit_button_callback, (void *) popup );
+    (void) add_widget_to_list( &popup->widgets, widget );
+}
---
+++ User_interface/widget_instances/meter.c
@@ -0,0 +1,59 @@
+#include  <def_user_interface.h>
+
+public  void  initialize_meter(
+    popup_struct     *popup,
+    int              x_size,
+    int              y_size,
+    Colour           background_colour,
+    Colour           colour,
+    polygons_struct  **meter_background,
+    polygons_struct  **meter )
+{
+    int                     x, y, window_x_size, window_y_size;
+    object_struct           *object;
+
+    /* create the background of the meter */
+
+    object = create_rectangle( background_colour );
+
+    *meter_background = (polygons_struct *) get_object_pointer( object );
+
+    G_get_window_size( popup->graphics.window, &window_x_size, &window_y_size );
+
+    x = (window_x_size - x_size) / 2;
+    y = Interface_y_spacing + Button_height +
+          (window_y_size - Interface_y_spacing - Button_height - y_size) / 2;
+
+    position_rectangle( *meter_background, x, y, x_size, y_size );
+    
+    add_object_to_viewport( &popup->graphics.graphics, 0,
+                            NORMAL_PLANES, object );
+
+    /* create the meter */
+
+    object = create_rectangle( colour );
+
+    *meter = (polygons_struct *) get_object_pointer( object );
+
+    position_rectangle( *meter, x, y, 0, 0 );
+
+    add_object_to_viewport( &popup->graphics.graphics, 0,
+                            NORMAL_PLANES, object );
+}
+
+public  void  set_meter_position(
+    popup_struct      *popup,
+    polygons_struct   *meter_background,
+    polygons_struct   *meter,
+    Real              fraction_done )
+{
+    int   x, y, x_size, y_size;
+
+    x = (int) Point_x(meter_background->points[0]);
+    y = (int) Point_y(meter_background->points[0]);
+    x_size = (int) Point_x(meter_background->points[1]) - x + 1;
+    y_size = (int) Point_y(meter_background->points[2]) - y + 1;
+
+    position_rectangle( meter, x, y, ROUND( fraction_done * x_size ), y_size );
+    set_viewport_update_flag( &popup->graphics.graphics, 0, NORMAL_PLANES );
+}
