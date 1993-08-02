83e8513222d751cbcc9b4afff271e317bd47be03
david <david>
1993-08-02 13:31:23 +0000

    Initial revision

---
+++ Functionality/Include/register.h
@@ -0,0 +1,12 @@
+#ifndef  DEF_REGISTER
+#define  DEF_REGISTER
+
+#include  <def_mni.h>
+
+#include  <def_main_struct.h>
+
+#include  <def_globals.h>
+
+#include  <def_Functionality_prototypes.h>
+
+#endif
