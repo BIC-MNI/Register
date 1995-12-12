8f17a8bfc4b5ac85a7a9e4e5b8a791881c496753
david <david>
1995-12-12 14:57:55 +0000

    check_in_all

--- Functionality/Include/global_variables.h
+++ Functionality/Include/global_variables.h
@@ -55,5 +55,5 @@ START_GLOBALS
     DEF_GLOBAL( Initial_slice_filter_type, int, 0 )
     DEF_GLOBAL( Initial_slice_filter_width, Real, 1.0 )
 
-    DEF_GLOBAL( Convert_vectors_to_rgb, BOOLEAN, TRUE )
+    DEF_GLOBAL( Convert_vectors_to_rgb, BOOLEAN, FALSE )
 END_GLOBALS
