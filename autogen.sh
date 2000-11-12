185b3e62dce4cd2d3bba9052aecd523408e6fa0a
stever <stever>
2000-11-12 16:35:37 +0000

    The commit intended to simply remove the "Build" directory accidentally committed some other changes.  This commit finishes that job.
    Add files required for autoconf-generated configure process.

---
+++ autogen.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+cat <<EOF
+Messages of the following type may be safely ignored.
+Any other diagnostics may be a sign of trouble.  
+Let us know if something goes wrong.
+
+    automake: configure.in: installing [...]
+    warning: AC_TRY_RUN called without default to allow cross compiling
+
+
+
+EOF
+
+aclocal
+autoheader
+automake --add-missing
+autoconf
+
