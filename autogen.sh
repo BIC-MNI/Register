a892418b59571edd0e47c96ca72c1f1c68a3c4da
stever <stever>
2001-01-26 23:37:01 +0000

    Polishing for release

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
 cat <<EOF
-Messages of the following type may be safely ignored.
-Any other diagnostics may be a sign of trouble.  
-Let us know if something goes wrong.
+Messages of the following type may be safely ignored:
 
     automake: configure.in: installing [...]
     warning: AC_TRY_RUN called without default to allow cross compiling
 
+Any other diagnostics may be a sign of trouble.  
+Let us know if something goes wrong.
 
 
 EOF
