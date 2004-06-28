011dd8d993224bd1013aba971fb0bb3f0e102af8
bert <bert>
2004-06-28 20:58:13 +0000

    Add check for m4 directory

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,19 @@
 #! /bin/sh
 
+if [ ! -r m4/mni_REQUIRE_LIB.m4 ]; then
+    cat <<EOF
+The required m4 files were not found.
+You need to check these out from their repository
+using
+
+    cvs -d /software/source checkout -d m4 libraries/mni-acmacros
+
+(yes, two '-d' options)
+Then re-run autogen.sh.
+EOF
+    exit 1
+fi
+
 set -e
 
 aclocal -I m4
