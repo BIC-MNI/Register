9b437995b3c2bf1dc1884c1f33e8db2de01f5ab4
Robert D. Vincent <robert.d.vincent@mcgill.ca>
2016-05-13 14:23:42 -0400

    Fix #12 - Change image saving method: use bicpl library call to create .ppm files rather than relying on ImageMagick.

--- Functionality/slices/save_image.c
+++ Functionality/slices/save_image.c
@@ -1,5 +1,7 @@
-/* ----------------------------------------------------------------------------
-@COPYRIGHT  :
+/**
+ * \file Functionality/slices/save_image.c
+ *
+ \copyright
               Copyright 1993,1994,1995 David MacDonald,
               McConnell Brain Imaging Centre,
               Montreal Neurological Institute, McGill University.
@@ -10,45 +12,62 @@
               make no representations about the suitability of this
               software for any purpose.  It is provided "as is" without
               express or implied warranty.
----------------------------------------------------------------------------- */
+ */
 
 #include  <register.h>
+#include  <bicpl/images.h>
 
 #define  PREFIX   "register_image_"
 
 static  int  frame_number = 1;
 
-static  void  save_rgb_image(
-    VIO_STR   filename,
-    int      x_min,
-    int      x_max,
-    int      y_min,
-    int      y_max )
+VIO_Status   save_window_to_file(
+    Gwindow         window,
+    VIO_STR         filename,
+    int             x_min,
+    int             x_max,
+    int             y_min,
+    int             y_max )
 {
-    char   command[VIO_EXTREMELY_LARGE_STRING_SIZE];
+    VIO_Status      status;
+    int             x_size, y_size;
+    pixels_struct   pixels;
 
-    (void) sprintf( command, "import -window root -crop %dx%d+%d+%d %s ", 
-                    x_max-x_min, y_max-y_min, x_min, y_min, filename );
-    (void) printf( "%s", command );
-    (void) flush_file( stdout );
-    (void) system( command );
-    (void) printf( "  ------ done\n" );
+    if( x_min > x_max || y_min > y_max )
+    {
+        G_get_window_size( window, &x_size, &y_size );
+        x_min = 0;
+        x_max = x_size-1;
+        y_min = 0;
+        y_max = y_size-1;
+    }
+    else
+    {
+        x_size = x_max - x_min + 1;
+        y_size = y_max - y_min + 1;
+    }
+    initialize_pixels( &pixels, 0, 0, x_size, y_size, 1.0, 1.0, RGB_PIXEL );
+    G_read_pixels( window, x_min, x_max, y_min, y_max,
+                   &PIXEL_RGB_COLOUR(pixels, 0, 0) );
+    status = output_rgb_file( filename, &pixels );
+    delete_pixels( &pixels );
+    return( status );
 }
 
-  void  save_image(
+void  save_image(
     main_struct   *main_info,
     int           volume_index,
     int           view_index )
 {
-    int     axis, sizes[VIO_MAX_DIMENSIONS];
-    VIO_Real    *position, voxel[VIO_MAX_DIMENSIONS];
-    VIO_Volume  volume;
-    VIO_Real    x_min_real, y_min_real, x_max_real, y_max_real;
-    int     x_min, y_min, x_max, y_max;
-    int     x_size, y_size;
-    int     viewport_x_min, viewport_y_min, viewport_x_max, viewport_y_max;
-    int     x_origin, y_origin;
-    char    filename[VIO_EXTREMELY_LARGE_STRING_SIZE];
+    int        axis, sizes[VIO_MAX_DIMENSIONS];
+    VIO_Real   *position, voxel[VIO_MAX_DIMENSIONS];
+    VIO_Volume volume;
+    VIO_Real   x_min_real, y_min_real, x_max_real, y_max_real;
+    int        x_min, y_min, x_max, y_max;
+    int        vp_x_min, vp_y_min, vp_x_max, vp_y_max;
+    int        x_size, y_size;
+    char       filename[VIO_EXTREMELY_LARGE_STRING_SIZE];
+    VIO_Status status;
 
     if( !is_volume_active( main_info, volume_index ) )
         return;
@@ -78,20 +97,27 @@ static  void  save_rgb_image(
     get_slice_viewport_size( main_info, volume_index, view_index,
                              &x_size, &y_size );
 
-    if (x_min_real < x_max_real) {
+    get_slice_viewport( main_info, volume_index, view_index, 
+                        &vp_x_min, &vp_x_max, &vp_y_min, &vp_y_max );
+
+    if (x_min_real < x_max_real)
+    {
         x_min = (int) x_min_real + 1;
         x_max = (int) x_max_real;
     }
-    else {
+    else
+    {
         x_min = (int) x_max_real + 1;
         x_max = (int) x_min_real;
     }
 
-    if (y_min_real < y_max_real) {
+    if (y_min_real < y_max_real)
+    {
         y_min = (int) y_min_real + 1;
         y_max = (int) y_max_real;
     }
-    else {
+    else
+    {
         y_min = (int) y_max_real + 1;
         y_max = (int) y_min_real;
     }
@@ -105,25 +131,18 @@ static  void  save_rgb_image(
     if( y_max >= y_size )
         y_max = y_size-1;
 
-    G_get_window_position( main_info->window, &x_origin, &y_origin );
-    G_get_window_size(main_info->window, &x_size, &y_size );
-
-    get_slice_viewport( main_info, volume_index, view_index,
-                        &viewport_x_min, &viewport_x_max,
-                        &viewport_y_min, &viewport_y_max );
-
-    /* Now invert the sense of the viewport w.r.t. the Y axis */
-    viewport_y_min = y_size - viewport_y_max;
-
     do
     {
-        (void) sprintf( filename, "%s%d.tiff", PREFIX, frame_number );
+        (void) sprintf( filename, "%s%d.ppm", PREFIX, frame_number );
         ++frame_number;
     }
     while( file_exists( filename ) );
 
-    save_rgb_image( filename, x_origin + viewport_x_min + x_min,
-                              x_origin + viewport_x_min + x_max,
-                              y_origin + viewport_y_min + y_min,
-                              y_origin + viewport_y_min + y_max );
+    status = save_window_to_file( main_info->window, filename,
+                                  vp_x_min+x_min, vp_x_min+x_max,
+                                  vp_y_min+y_min, vp_y_min+y_max);
+    if (status == VIO_OK)
+    {
+      printf( "Saved image to '%s'.\n", filename );
+    }
 }
